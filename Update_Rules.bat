2820600967b44d44b66b6f7cd1fd2867da1aa55c
Thomas Blasquez <31079038+RingComics@users.noreply.github.com>
2021-06-01 14:16:34 -0700

    Update_Rules.bat also updates mlox_base.txt

--- Update_Rules.bat
+++ Update_Rules.bat
@@ -1,1 +1,2 @@
 curl https://raw.githubusercontent.com/DanaePlays/mlox-rules/main/mlox_user.txt -o mlox_user.txt
+curl https://raw.githubusercontent.com/DanaePlays/mlox-rules/main/mlox_base.txt -o mlox_base.txt
